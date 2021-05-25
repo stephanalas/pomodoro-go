85c8fe5579c5d5486cb0d43ea0e2915a4b5eb06e
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-05-25 13:55:33 -0400

    cleaned up tests

--- server/db/models/session.spec.js
+++ server/db/models/session.spec.js
@@ -12,16 +12,6 @@ describe('Session model', () => {
 
     sessions = (await seed()).sessions;
   });
-
-  it('`createWithUser` class method creates a session with an associated user', async () => {
-    //start here
-    const session3 = await Session.createWithUser({
-      userId: 1,
-      sessionTime: 30,
-    });
-    console.log('session3:', session3);
-    // expect(session0.sessionTime).to.equal(40);
-  });
   it('requires a sessionTime', () => {
     //start here
     const { session0, session1 } = sessions;
@@ -29,12 +19,23 @@ describe('Session model', () => {
     expect(session0.sessionTime).to.equal(40);
     expect(session1.sessionTime).to.equal(50);
   });
-  it('has a hook that calculates an expectedEndTime as instances are created', () => {
+  it('`calcExpectedEndTime` method calculates and adds expectedEndTime attribute to instances as they are created', () => {
     const { session0 } = sessions;
     const expectedEndTime = session0.expectedEndTime;
     const check = new Date(
       Date.parse(session0.createdAt) + session0.sessionTime * 60000
     );
     expect(Date.parse(expectedEndTime)).to.equal(Date.parse(check));
+  });
+  it('`createWithUser` class method creates a session with an associated user', async () => {
+    //start here
+    const session3 = await Session.createWithUser({
+      userId: 1,
+      sessionTime: 30,
+    });
+
+    expect(session3.userId).to.equal(1);
+    expect(session3.sessionTime).to.equal(30);
+    expect(session3.expectedEndTime).to.be.ok;
   });
 });
