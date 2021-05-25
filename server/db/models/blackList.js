682d6176792d3f85e60bad19c95f1b06153f2c52
Yi-Ru Ding <dingyr0925@gmail.com>
2021-05-25 19:39:40 -0400

    7:39PM

--- server/db/models/blackList.js
+++ server/db/models/blackList.js
@@ -1,15 +1,6 @@
 const { UUID, UUIDV4, STRING, INTEGER } = require('sequelize');
 const db = require('../db');
 
-const Blacklist = db.define('blackList', {
-  userId: {
-    type: UUID,
-    defaultValue: UUIDV4,
-  },
-  siteId: {
-    type: UUID,
-    defaultValue: UUIDV4,
-  },
-});
+const Blacklist = db.define('blackList', {});
 
 module.exports = Blacklist;
