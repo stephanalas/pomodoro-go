16b4661ea8c96a5d32cf9239dd2e0eb692b96a21
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-06-05 21:51:07 -0400

    updated  Dashboard component

--- server/api/sessions.js
+++ server/api/sessions.js
@@ -1,39 +1,40 @@
-const router = require('express').Router()
-const { models: { Session, User }} = require('../db')
-module.exports = router
+const router = require('express').Router();
+const {
+  models: { Session, User },
+} = require('../db');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
     const sessions = await Session.findAll({
-      include: [ User ]
+      include: [User],
     });
-    res.send(sessions)
+    res.send(sessions);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.get('/:sessionId', async (req, res, next) => {
   try {
     const session = await Session.findByPk(req.params.sessionId, {
-      include: [ User ]
+      include: [User],
     });
-    res.send(session)
+    res.send(session);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.get('/user/:userId', async (req, res, next) => {
   try {
     const userSessions = await Session.findAll({
       where: {
-        userId: req.params.userId
-      }
-    })
-    res.send(userSessions)
+        userId: req.params.userId,
+      },
+    });
+    res.send(userSessions);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
-
+});
