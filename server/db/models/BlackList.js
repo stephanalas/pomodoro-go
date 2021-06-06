350d9008b1136fed4208d2eb3580b47028cebb68
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-06-06 10:49:31 -0400

    .gitignore should now be working

--- server/db/models/BlackList.js
+++
@@ -1,7 +0,0 @@
-const { UUID, UUIDV4, STRING, INTEGER } = require('sequelize');
-const db = require('../db');
-
-// association table
-const BlackList = db.define('blacklist', {});
-
-module.exports = BlackList;
