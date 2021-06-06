d6aaeca39b98759232f97330d3228b0a1a1e2966
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-06-06 11:00:17 -0400

    fixing travis issue

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -12,7 +12,6 @@ import TotalSessions from './TotalSessions';
 import AverageSession from './AverageSession';
 import DayOfWeekChart from './DayOfWeekChart';
 import HeatMap from './HeatMap';
-//test test
 const Dashboard = () => {
   let sessions = useSelector((state) => state.sessions);
   let goals = useSelector((state) => state.goals);
