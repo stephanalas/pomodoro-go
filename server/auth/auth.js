f9ceb7afba114d6906ac8b1d607bc43aaf20917e
Stephan Alas <stephan.j.alas@gmail.com>
2021-06-10 00:09:29 -0400

    added passport config; local and jwt strategy

---
+++ server/auth/auth.js
@@ -0,0 +1,68 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const {
+  models: { User },
+} = require('../db');
+passport.use(
+  'jwt',
+  new JwtStrategy(
+    {
+      secretOrKey: process.env.JWT,
+      jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    },
+    async (token, done) => {
+      try {
+        console.log(token);
+        User.findOne({ id: token.sub }, function (err, user) {
+          if (err) {
+            return done(err, false);
+          }
+          if (user) {
+            console.log(user);
+            return done(null, user);
+          }
+          console.log('no user exist or maybe error');
+          return done(null, false);
+        });
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, (username, password, done) => {
+    User.findOne({ where: { email: username } }).then(async (user) => {
+      try {
+        console.log('hello');
+        if (!user) {
+          return done(null, false, { message: 'Email not found' });
+        } else if (!user.correctPassword(password)) {
+          return done(null, false, { message: 'Incorrect password' });
+        } else {
+          return done(null, await user.generateToken());
+        }
+      } catch (error) {
+        console.log('error from local strategy authentication', error);
+        done(error, false);
+      }
+    });
+  })
+);
+
+passport.serializeUser((token, done) => {
+  done(null, token);
+});
+
+passport.deserializeUser(async (token, done) => {
+  try {
+    done(null, await User.findByToken(token));
+  } catch (error) {
+    done(error, false);
+  }
+});
+module.exports = passport;
