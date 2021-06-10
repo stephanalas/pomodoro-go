0c86c4033966d32b1ba5e80b065080d0ad30f8c2
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-06-10 14:34:57 -0400

    reverted back from useEffect

--- client/components/Dashboard/LastSession.js
+++ client/components/Dashboard/LastSession.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Typography, Paper, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
