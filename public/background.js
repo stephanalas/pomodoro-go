f529627aa115dfecb7b684bf01d248d160bbe175
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-17 18:31:08 -0400

    redirect to localhost on new tabs

--- public/background.js
+++ public/background.js
@@ -2,11 +2,21 @@
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
   console.log(tabId);
+  console.log(changeInfo);
   const url = changeInfo.pendingUrl || changeInfo.url;
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    let url = tabs[0].url;
+    console.log(url);
+    if (url === 'chrome://newtab/') {
+      chrome.tabs.update(tabId, { url: 'http://localhost:8080/' });
+    }
+  });
+
   if (!url || !url.startsWith('http')) {
+    console.log('no url');
+    // chrome.tabs.update(tabId, { url: 'http://localhost:8080/' });
     return;
   }
-
   const hostname = new URL(url).hostname;
   console.log('hostname:', hostname);
   chrome.storage.local.set({ userAttempt: hostname });
