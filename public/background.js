def99723d0bef91559cd19c5e7389c67f8c55a43
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-19 13:54:41 -0400

    chrome extension code added

--- public/background.js
+++ public/background.js
@@ -1,27 +1,72 @@
 'use strict';
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
-  console.log(tabId);
-  console.log(changeInfo);
-  const url = changeInfo.pendingUrl || changeInfo.url;
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-    let url = tabs[0].url;
-    console.log(url);
-    if (url === 'chrome://newtab/') {
-      chrome.tabs.update(tabId, { url: 'http://localhost:8080/' });
+const { storage, tabs } = chrome;
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  try {
+    if (message === 'app-starting') {
+      sendResponse('clearing alarms and storage');
+      await chrome.storage.sync.clear();
+      await chrome.alarms.clearAll();
+      chrome.storage.sync.set({
+        sessionTime: 0,
+        timerOn: false,
+        currentSession: null,
+        alarmCreated: false,
+      });
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+storage.onChanged.addListener(async function (changes, namespace) {
+  try {
+    let state = {};
+    chrome.storage.sync.get(null, (results) => {
+      for (const i in results) {
+        state[i] = results[i];
+      }
+    });
+
+    if (
+      changes.currentSession &&
+      changes.currentSession.newValue.startTime &&
+      !state.alarmCreated
+    ) {
+      chrome.alarms.create('timer', {
+        when: Date.now() + changes.currentSession.newValue.sessionTime,
+      });
+      chrome.storage.sync.set({ alarmCreated: true });
+    }
+
+    // logging out the changes in storage
+
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+      console.log(
+        `Storage key "${key}" in namespace "${namespace}" changed.`,
+        `Old value was "${JSON.stringify(
+          oldValue
+        )}", new value is "${JSON.stringify(newValue)}".`
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+tabs.onUpdated.addListener(function (tabId, changeInfo) {
+  const url = changeInfo.pendingUrl || changeInfo.url;
 
   if (!url || !url.startsWith('http')) {
-    console.log('no url');
-    // chrome.tabs.update(tabId, { url: 'http://localhost:8080/' });
     return;
   }
   const hostname = new URL(url).hostname;
   console.log('hostname:', hostname);
-  chrome.storage.local.set({ userAttempt: hostname });
 
-  chrome.storage.local.get(['blocked'], function (local) {
+  storage.local.set({ userAttempt: hostname });
+
+  storage.local.get(['blocked'], function (local) {
     const { blocked } = local;
     if (
       Array.isArray(blocked) &&
@@ -30,8 +75,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
         return domain.includes(hostname);
       })
     ) {
-      // chrome.tabs.remove(tabId);
-      chrome.tabs.update(tabId, { url: 'http://localhost:8080/uhoh' });
+      tabs.update(tabId, { url: 'http://localhost:8080/uhoh' });
     }
   });
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+  console.log('got an alarm!', alarm);
 });
