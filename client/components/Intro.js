cbdf1ab2bc91ebb44387574fd14e133b1e6823d2
Yi-Ru Ding <dingyr0925@gmail.com>
2021-06-21 20:07:16 -0400

    Added Intro in routes.js,changed the color

--- client/components/Intro.js
+++ client/components/Intro.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import Typical from 'react-typical';
-import { makeStyles } from '@material-ui/core';
-
 
 export default class Intro extends Component {
   render() {
     return (
-        <div id='Intro'>
-      <Typical 
-        steps={[
-          'Hi,this is the App that helps boots your work effecientcy by blocking time-wasting websites and bringing in your favorite music.Come set the goal,hit the road!',
-          800,
-        ]}
-        loop={Infinity}
-        wrapper="b"
-      />
+      <div id="Intro">
+        <Typical
+          steps={[
+            'Hi,this is the App that helps boots your work effecientcy by blocking time-wasting websites and bringing in your favorite music.Come set the goal,hit the road!',
+            800,
+          ]}
+          loop={Infinity}
+          wrapper="b"
+        />
       </div>
     );
   }
