37c70cfbf35ea4b4bed5118da6a1f99742443635
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-06-24 14:50:21 -0400

    partway through hours

--- client/components/Dashboard/ChartLeft.js
+++ client/components/Dashboard/ChartLeft.js
@@ -91,14 +91,14 @@ const ChartLeft = (props) => {
     return dayOfWeek;
   });
 
-  console.log('sessionsSuccessful', sessionsSuccessful);
+
   const distDaysSuccessful = { Sun: 0, Mon: 0, Tue: 0, Wed: 0, Thu: 0, Fri: 0, Sat: 0 };
   if (sessionsSuccessful.length) {
     for (let i = 0; i < sessionDaysSuccessful.length; i++) {
       distDaysSuccessful[sessionDaysSuccessful[i]]++;
     }
   }
-  console.log('distDaysSuccessful', distDaysSuccessful);
+
 
   let valsArrSuccessful = [];
   for (const [key, val] of Object.entries(distDaysSuccessful)) {
@@ -114,14 +114,14 @@ const ChartLeft = (props) => {
     return dayOfWeek;
   });
 
-  console.log('sessionsFailed', sessionsFailed);
+
   const distDaysFailed = { Sun: 0, Mon: 0, Tue: 0, Wed: 0, Thu: 0, Fri: 0, Sat: 0 };
   if (sessionsFailed.length) {
     for (let i = 0; i < sessionDaysFailed.length; i++) {
       distDaysFailed[sessionDaysFailed[i]]++;
     }
   }
-  console.log('distDaysFailed', distDaysFailed);
+
 
   let valsArrFailed = [];
   for (const [key, val] of Object.entries(distDaysFailed)) {
@@ -269,6 +269,112 @@ const ChartLeft = (props) => {
     categories: hoursArr,
   };
 
+  //Successful Sessions for Stacked Hours Distribution View
+
+  const sessionHoursSuccessful = sessionsSuccessful.map((session) => {
+    const hour = dayjs(session.startTime).format('H');
+    return hour;
+  });
+
+  console.log('sessionsSuccessful', sessionsSuccessful);
+  const distHoursSuccessful = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 0,
+    13: 0,
+    14: 0,
+    15: 0,
+    16: 0,
+    17: 0,
+    18: 0,
+    19: 0,
+    20: 0,
+    21: 0,
+    22: 0,
+    23: 0,
+  };
+
+  if (sessionsSuccessful.length) {
+    for (let i = 0; i < sessionHoursSuccessful.length; i++) {
+      distHoursSuccessful[sessionHoursSuccessful[i]]++;
+    }
+  }
+  console.log('distHoursSuccessful', distHoursSuccessful);
+
+  let hourValsArrSuccessful = [];
+  for (const [key, val] of Object.entries(distHoursSuccessful)) {
+    hourValsArrSuccessful.push(val);
+  }
+
+  //Failed Sessions for Stacked Distribution View
+
+  const sessionHoursFailed = sessionsFailed.map((session) => {
+    const hour = dayjs(session.startTime).format('H');
+    return hour;
+  });
+
+  const distHoursFailed = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 0,
+    13: 0,
+    14: 0,
+    15: 0,
+    16: 0,
+    17: 0,
+    18: 0,
+    19: 0,
+    20: 0,
+    21: 0,
+    22: 0,
+    23: 0,
+  };
+  if (sessionsFailed.length) {
+    for (let i = 0; i < sessionHoursFailed.length; i++) {
+      distHoursFailed[sessionHoursFailed[i]]++;
+    }
+  }
+  console.log('distHoursFailed', distHoursFailed);
+
+  let hourValsArrFailed = [];
+  for (const [key, val] of Object.entries(distHoursFailed)) {
+    valsArrFailed.push(val);
+  }
+
+  const stackedDataHours = {
+    series: [
+      {
+        name: 'Failed',
+        data: hourValsArrFailed,
+      },
+      {
+        name: 'Successful',
+        data: hourValsArrSuccessful,
+      },
+
+    ],
+  };
+
   const hourChart = {
     options: {
       chart: {
@@ -278,7 +384,7 @@ const ChartLeft = (props) => {
           show: true,
         },
       },
-      colors: [primary.main],
+      colors: stacked ? [error.main, primary.main ]: [primary.main],
       dataLabels: {
         enabled: false,
       },
@@ -371,7 +477,7 @@ const ChartLeft = (props) => {
         },
       },
     },
-    series: hourData.series,
+    series: stacked ? stackedDataHours.series : hourData.series,
   };
 
   //Distribution By Goals Chart
