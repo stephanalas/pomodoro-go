e890b019e73a2b1482f793b59bd6548e59093377
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-23 22:03:26 -0400

    fixed errors

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -37,104 +37,108 @@ const Nav = (props) => {
     props.handleClick();
     localStorage.setItem('user', {});
   };
+  const { isLoggedIn, classes } = props;
+  if (!!isGoogleLoggedIn) {
+    return <GLogout />;
+  } else {
+    return (
+      <div>
+        <nav id="navBar">
+          {chrome.storage ? (
+            <AppBar position="relative" className={classes.header}>
+              <Toolbar>
+                <Typography variant="h4">Pomodoro,go!</Typography>
+              </Toolbar>
+            </AppBar>
+          ) : (
+            <AppBar position="relative" className={classes.header}>
+              <Toolbar>
+                <IconButton
+                  id="home"
+                  aria-label="home"
+                  edge="start"
+                  size="medium"
+                  component={Link}
+                  to="/home"
+                >
+                  <HomeOutlined />
+                </IconButton>
+                <IconButton
+                  id="account"
+                  aria-label="menu"
+                  aria-haspopup="true"
+                  edge="start"
+                  size="medium"
+                  onClick={(ev) => setAnchorEl(ev.currentTarget)}
+                  // ref={anchorRef}
+                >
+                  <AccountBox />
+                </IconButton>
+                {/* || this.state.isGoogleLogedIn  */}
+                {isLoggedIn ? (
+                  <Menu
+                    id="menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    aria-haspopup="true"
+                    onClose={() => setAnchorEl(null)}
+                  >
+                    <MenuItem
+                      onClick={handleLogOut}
+                      component={Link}
+                      to="/timer"
+                    >
+                      Timer
+                    </MenuItem>
+
+                    <MenuItem onClick={handleLogOut}>Logout</MenuItem>
+                  </Menu>
+                ) : (
+                  <Menu
+                    id="menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    aria-haspopup="true"
+                    onClose={() => setAnchorEl(null)}
+                  >
+                    <MenuItem
+                      key="Login"
+                      component={Link}
+                      onClick={() => setAnchorEl(null)}
+                      to="/login"
+                    >
+                      Login
+                    </MenuItem>
+                    <MenuItem
+                      key="SignUp"
+                      onClick={() => setAnchorEl(null)}
+                      component={Link}
+                      to="/signup"
+                    >
+                      Sign Up
+                    </MenuItem>
+                  </Menu>
+                )}
+
+                <Typography variant="h4">Pomodoro,go!</Typography>
+                {/* {isGoogleLogedIn ? <GLogout /> : <GLogin />} */}
+                {isLoggedIn ? (
+                  <Button onClick={handleLogOut}>Sign Out</Button>
+                ) : (
+                  <Button className="login" onClick={handleLogIn}>
+                    Log In
+                  </Button>
+                )}
+              </Toolbar>
+            </AppBar>
+          )}
+        </nav>
+      </div>
+    );
+  }
 };
-const { isLoggedIn, classes } = props;
-if (!!isGoogleLoggedIn) {
-  return <GLogout />;
-} else {
-  return (
-    <div>
-      <nav id="navBar">
-        {chrome.storage ? (
-          <AppBar position="relative" className={classes.header}>
-            <Toolbar>
-              <Typography variant="h4">Pomodoro,go!</Typography>
-            </Toolbar>
-          </AppBar>
-        ) : (
-          <AppBar position="relative" className={classes.header}>
-            <Toolbar>
-              <IconButton
-                id="home"
-                aria-label="home"
-                edge="start"
-                size="medium"
-                component={Link}
-                to="/home"
-              >
-                <HomeOutlined />
-              </IconButton>
-              <IconButton
-                id="account"
-                aria-label="menu"
-                aria-haspopup="true"
-                edge="start"
-                size="medium"
-                onClick={(ev) => setAnchorEl(ev.currentTarget)}
-                // ref={anchorRef}
-              >
-                <AccountBox />
-              </IconButton>
-              {/* || this.state.isGoogleLogedIn  */}
-              {isLoggedIn ? (
-                <Menu
-                  id="menu"
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl)}
-                  aria-haspopup="true"
-                  onClose={() => setAnchorEl(null)}
-                >
-                  <MenuItem onClick={handleLogOut} component={Link} to="/timer">
-                    Timer
-                  </MenuItem>
-
-                  <MenuItem onClick={handleLogOut}>Logout</MenuItem>
-                </Menu>
-              ) : (
-                <Menu
-                  id="menu"
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl)}
-                  aria-haspopup="true"
-                  onClose={() => setAnchorEl(null)}
-                >
-                  <MenuItem
-                    key="Login"
-                    component={Link}
-                    onClick={() => setAnchorEl(null)}
-                    to="/login"
-                  >
-                    Login
-                  </MenuItem>
-                  <MenuItem
-                    key="SignUp"
-                    onClick={() => setAnchorEl(null)}
-                    component={Link}
-                    to="/signup"
-                  >
-                    Sign Up
-                  </MenuItem>
-                </Menu>
-              )}
-
-              <Typography variant="h4">Pomodoro,go!</Typography>
-              {/* {isGoogleLogedIn ? <GLogout /> : <GLogin />} */}
-              {isLoggedIn ? (
-                <Button onClick={handleLogOut}>Sign Out</Button>
-              ) : (
-                <Button className="login" onClick={handleLogIn}>
-                  Log In
-                </Button>
-              )}
-            </Toolbar>
-          </AppBar>
-        )}
-      </nav>
-    </div>
-  );
-}
 /**
  * CONTAINER
  */
