065f0b810e5b3048d5040e221c228a58efe5b49c
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-23 21:55:29 -0400

    removed code

--- client/app.js
+++ client/app.js
@@ -1,32 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Nav from './components/Nav';
 import Routes from './routes';
 import { makeStyles } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 const useStyles = makeStyles(() => ({
   main: {
     height: '100%',
     width: '100%',
+    backgroundColor: '#e4ddee',
   },
 }));
 const App = () => {
   const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (chrome.runtime) {
-      chrome.storage.sync.get(['user'], (result) => {
-        if (result.user) {
-          console.log('user in sync', result.user);
-          dispatch({ type: 'SET_AUTH', auth: result.user });
-        }
-      });
-      chrome.runtime.sendMessage('app-starting', (response) => {
-        console.log('message from chrome', response);
-      });
-    }
-  }, [dispatch]);
   return (
     <div className={classes.main}>
       <Nav />
