c89dd04b47d523403000a59e5bfb8287d869d269
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-23 21:52:01 -0400

    removed unnecessary code

--- client/components/Timer/CreateSession.js
+++ client/components/Timer/CreateSession.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useRef } from 'react';
+import React, { createContext, useState } from 'react';
 import { makeStyles, Container, Paper } from '@material-ui/core';
 import Timer from './Timer';
 import FocusConfig from './FocusConfig';
