7ac9a06994891c22f055e20f2b7a300ef1702363
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-25 01:59:06 -0400

    might toss this code

--- public/localStorage.js
+++ public/localStorage.js
@@ -13,14 +13,35 @@
 //   }
 // };
 
-window.onload = async () => {
-  const sessionTime = localStorage.getItem('sessionTime');
-  console.log('windowONload sessiontime,', sessionTime);
-  if (sessionTime >= 0 && sessionTime) {
-    chrome.storage.sync.set({ sessionTime: Number(sessionTime) });
-    chrome.storage.sync.get(['sessionTime'], (results) => {
-      console.log('grabbed sessionTime from chromeStorage', results);
+window.onload = () => {
+  console.log('window just loaded');
+  const currentSessionTime = window.localStorage.getItem('sessionTime');
+  if (parseInt(currentSessionTime)) {
+    // chrome.runtime.sendMessage({
+    //   message: 'continue-alarm',
+    //   sessionTime: parseInt(currentSessionTime),
+    // });
+    // window.timer = setInterval(() => {
+    //   const newSessionTime = parseInt(localStorage.getItem('sessionTime'));
+    //   if (!newSessionTime) {
+    //     console.log('timer reached zero yayyyyy!!!');
+    //     clearInterval();
+    //   } else {
+    //     localStorage.setItem('sessionTime', newSessionTime - 1000);
+    //   }
+    // }, 1000);
+  } else {
+    clearInterval(window.timer);
+  }
+};
+window.onbeforeunload = () => {
+  const currentSessionTime = window.localStorage.getItem('sessionTime');
+  const timerDone = window.localStorage.getItem('timerDone');
+  if (!timerDone && parseInt(currentSessionTime)) {
+    clearInterval(window.timer);
+    chrome.runtime.sendExternalMessage('jgphbioennmnjogfbpchcgphelmfoiig', {
+      message: 'create-alarm',
+      sessionTime: parseInt(currentSessionTime),
     });
   }
-  cons;
 };
