b27069fe9487cc0208952a2f1571cb899d0d02b8
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-25 02:02:18 -0400

    replaced session context

--- client/app.js
+++ client/app.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 
 import Nav from './components/Nav';
 import Routes from './routes';
 import { makeStyles } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
+import { me } from './store';
+export const SessionContext = createContext();
+
 const useStyles = makeStyles(() => ({
   main: {
     height: '100%',
@@ -14,21 +17,60 @@ const useStyles = makeStyles(() => ({
 const App = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const currentSession = useSelector((state) => state.currentSession);
+  const auth = useSelector((state) => state.auth);
+  const [sessionTime, setSessionTime] = useState(0);
+  const [goal, setGoal] = useState('');
+  const [countDown, setCountDown] = useState(false);
   useEffect(() => {
-    if (chrome.runtime) {
-      chrome.storage.sync.get(['user'], (result) => {
-        if (result.user) {
-          console.log('user in sync', result.user);
-          dispatch({ type: 'SET_AUTH', auth: result.user });
-        }
+    // setPort(PORT);
+    const sT = window.localStorage.getItem('sessionTime');
+    if (parseInt(sT)) {
+      // setSessionTime((sessionTime) => {
+      //   const newSessionTime = sessionTime - 1000;
+      //   localStorage.setItem('sessionTime', newSessionTime);
+      //   return newSessionTime;
+      // });
+    }
+    if (!parseInt(sT)) {
+      if (window.timer) window.localStorage.setItem('timerDone', true);
+      console.log('timer is over');
+      clearInterval(window.timer);
+
+      window.localStorage.setItem('sessionTime', 0);
+    }
+    if (
+      window.localStorage.getItem('timerDone') === 'true' &&
+      localStorage.getItem('currentSession') !== 'null'
+    ) {
+      console.log('timer done is true');
+      chrome.runtime.sendMessage('jgphbioennmnjogfbpchcgphelmfoiig', {
+        message: 'timer-done',
       });
+      window.localStorage.setItem('currentSession', null);
+    }
+    console.log('app is refreshing');
+  });
+  useEffect(() => {
+    if (window.localStorage.getItem('token')) {
+      dispatch(me());
     }
   }, [dispatch]);
-
   return (
     <div className={classes.main}>
-      <Nav />
-      <Routes />
+      <SessionContext.Provider
+        value={{
+          sessionTime,
+          setSessionTime,
+          goal,
+          setGoal,
+          countDown,
+          setCountDown,
+        }}
+      >
+        <Nav />
+        <Routes />
+      </SessionContext.Provider>
     </div>
   );
 };
