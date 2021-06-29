c7028c0d23c6b9d60395a927f5131c7d0a429e63
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-06-28 20:53:01 -0400

    created timer context and logic for timer

--- client/components/Timer/CreateSession.js
+++ client/components/Timer/CreateSession.js
@@ -1,26 +1,24 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState, useContext } from 'react';
 import { makeStyles, Container, Grid } from '@material-ui/core';
 import Timer from './Timer';
 import FocusConfig from './FocusConfig';
 import { connect, useSelector } from 'react-redux';
 import { loadSession } from '../../store/sessions';
+import { SessionContext } from '../../app';
+export const TimerContext = createContext();
+
 const useStyles = makeStyles(() => ({
   main: {
     display: 'flex',
     alignContent: 'left',
     justifyContent: 'center',
-    // border: '1px solid red',
     height: '100%',
     width: '100%',
   },
   paper: {
     display: 'flex',
     alignContent: 'center',
-    // border: '1px solid red',
     width: '100%',
-    height: '100%',
-    // display: 'flex',
-    // flexDirection: 'column',
     height: '100%',
     margin: '25px',
   },
@@ -34,18 +32,59 @@ const useStyles = makeStyles(() => ({
 }));
 const CreateSession = (props) => {
   const classes = useStyles();
-
+  const currentSession = useSelector((state) => state.currentSession);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const { setSessionTime, sessionTime, countDown, setCountDown } =
+    useContext(SessionContext);
+  useEffect(() => {
+    if (!countDown) {
+      const sec = seconds * 1000;
+      const min = minutes * 60000;
+      const hour = hours * 3600000;
+      setSessionTime(sec + min + hour);
+      window.localStorage.setItem('sessionTime', sec + min + hour);
+    } else {
+      window.timer = setInterval(() => {
+        setSessionTime((sessionTime) => {
+          const newSessionTime = sessionTime - 1000;
+          localStorage.setItem('sessionTime', newSessionTime);
+          return newSessionTime;
+        });
+        if (!sessionTime) {
+          setSessionTime(0);
+          setCountDown(false);
+          clearInterval(window.timer);
+        }
+      }, 1000);
+      return () => {
+        clearInterval(window.timer);
+      };
+    }
+  });
   return (
-    <Container className={classes.main}>
-      <Grid container justify="center">
-        <Grid item className={classes.focus}>
-          <FocusConfig />
+    <TimerContext.Provider
+      value={{
+        hours,
+        setHours,
+        minutes,
+        setMinutes,
+        seconds,
+        setSeconds,
+      }}
+    >
+      <Container className={classes.main}>
+        <Grid container justify="center">
+          <Grid item className={classes.focus}>
+            <FocusConfig />
+          </Grid>
+          <Grid item className={classes.timer}>
+            <Timer />
+          </Grid>
         </Grid>
-        <Grid item className={classes.timer}>
-          <Timer />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </TimerContext.Provider>
   );
 };
 export default connect(null, (dispatch) => {
