f7a53407842b3095306936a247f5db1e34cf8741
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-07-17 19:14:57 -0400

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,15 @@ const dotenv = require('dotenv');
 const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path');
-
 const currentPath = path.join(__dirname);
 const basePath = currentPath + '/.env';
 const envPath = basePath + '.' + process.env.NODE_ENV;
 const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-
 const fileEnv = dotenv.config({ path: finalPath }).parsed;
-
 const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
   return prev;
 }, {});
-
 module.exports = {
   entry: ['./client/index.js'],
   output: {
