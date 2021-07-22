7116dbeda31854f4829a7245eb0a31dcd6667804
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-07-21 20:45:58 -0400

    gets sessionTime and end session at correct time.

--- client/app.js
+++ client/app.js
@@ -5,7 +5,7 @@ import Routes from './routes';
 import { makeStyles } from '@material-ui/core';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { me } from './store';
-import { endSession, removeSession } from './store/sessions';
+import { endSession, loadSession, removeSession } from './store/sessions';
 export const SessionContext = createContext();
 
 const useStyles = makeStyles(() => ({
@@ -23,28 +23,56 @@ const App = (props) => {
   const [goal, setGoal] = useState('');
   const [countDown, setCountDown] = useState(false);
   const [intervalID, setIntervalID] = useState('');
+
   useEffect(() => {
     const timeLeft = localStorage.getItem('sessionTime');
     if (parseInt(timeLeft) < 0) return;
+
     if (!parseInt(timeLeft) && currentSession.id && countDown) {
       props.endSession(currentSession.id, true);
     }
   }, [sessionTime]);
+
   useEffect(() => {
-    if ((sessionTime, countDown)) {
-      const id = setInterval(() => {
-        setSessionTime((sessionTime) => {
-          localStorage.setItem('sessionTime', sessionTime - 1000);
-          return sessionTime - 1000;
-        });
-      }, 1000);
-      setIntervalID(id);
+    const timeFromStorage = JSON.parse(localStorage.getItem('currentSession'));
+    const sessionFromStorage = JSON.parse(
+      localStorage.getItem('currentSession')
+    );
+    const sessionActive = JSON.parse(localStorage.getItem('sessionActive'));
+    if (timeFromStorage <= 0 && sessionActive) {
+      props.endSession(sessionFromStorage.id, true);
+      localStorage.setItem('sessionActive', false);
     }
-    if (!countDown) {
-      clearInterval(intervalID);
-      setSessionTime(0);
-    }
+    // console.log('from app', sessionFromLocalStorage);
+    // if (sessionFromLocalStorage) {
+    //   dispatch(loadSession(sessionFromLocalStorage.id));
+    // }
+    // if ((sessionTime, countDown)) {
+    //   const id = setInterval(() => {
+    //     setSessionTime((sessionTime) => {
+    //       localStorage.setItem('sessionTime', sessionTime - 1000);
+    //       return sessionTime - 1000;
+    //     });
+    //   }, 1000);
+    //   setIntervalID(id);
+    // }
+    // if (!countDown) {
+    //   clearInterval(intervalID);
+    //   setSessionTime(0);
+    // }
+    chrome.runtime.sendMessage(
+      'opechfjocpfdfihnebpmdbkajmmomihl',
+      {
+        message: 'get-time',
+      },
+      (response) => {
+        console.log(response);
+        localStorage.setItem('sessionTime', response.sessionTime);
+        setSessionTime(response.sessionTime);
+      }
+    );
   }, [dispatch]);
+
   useEffect(() => {
     if (window.localStorage.getItem('token')) {
       dispatch(me());
