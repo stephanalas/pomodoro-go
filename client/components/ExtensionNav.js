078410a9bc0a6e2e2d78115995287d06518fe982
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-07-25 17:53:35 -0400

    nav for extension

---
+++ client/components/ExtensionNav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+const ExtensionNav = (props) => {
+  return (
+    <AppBar
+      position="static"
+      className={props.style}
+      style={{ backgroundColor: '#5061a9' }}
+    >
+      <Toolbar>
+        <Typography id="pomo-go" align="center" variant="h4">
+          Pomodoro,go!
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default ExtensionNav;
