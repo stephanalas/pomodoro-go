ee9ab62064bf46eb06ad7c927b06fddc19b0903d
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-07-26 21:39:09 -0400

    drying out code and adding comments

--- client/app.js
+++ client/app.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, createContext } from 'react';
-
-import Nav from './components/Nav';
-import Routes from './routes';
 import { makeStyles } from '@material-ui/core';
 import { connect, useDispatch, useSelector } from 'react-redux';
+import Nav from './components/Nav';
+import Routes from './routes';
 import { me } from './store';
-import { endSession, loadSession, removeSession } from './store/sessions';
+import { endSession, loadSession } from './store/sessions';
+
 export const SessionContext = createContext();
 
 const useStyles = makeStyles(() => ({
@@ -14,64 +14,52 @@ const useStyles = makeStyles(() => ({
     width: '100%',
   },
 }));
+
 const App = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const currentSession = useSelector((state) => state.currentSession);
-  const auth = useSelector((state) => state.auth);
   const [sessionTime, setSessionTime] = useState(0);
   const [goal, setGoal] = useState('');
-  const [countDown, setCountDown] = useState(false);
   const [intervalID, setIntervalID] = useState('');
+  const sessionActive = JSON.parse(localStorage.getItem('sessionActive'));
+  const timeFromStorage = JSON.parse(localStorage.getItem('currentSession'));
+  const sessionFromStorage = JSON.parse(localStorage.getItem('currentSession'));
 
   useEffect(() => {
-    const timeLeft = localStorage.getItem('sessionTime');
-    if (parseInt(timeLeft) < 0) return;
-
-    if (!parseInt(timeLeft) && currentSession.id && countDown) {
-      props.endSession(currentSession.id, true);
-    }
-  }, [sessionTime]);
-
-  useEffect(() => {
-    const timeFromStorage = JSON.parse(localStorage.getItem('currentSession'));
-    const sessionFromStorage = JSON.parse(
-      localStorage.getItem('currentSession')
-    );
-    const sessionActive = JSON.parse(localStorage.getItem('sessionActive'));
-    if (timeFromStorage <= 0 && sessionActive) {
+    // end session if time for storage is zero (during active session)
+    if (timeFromStorage <= 0 && sessionActive && sessionFromStorage) {
       props.endSession(sessionFromStorage.id, true);
-      localStorage.setItem('sessionActive', false);
     }
-    // console.log('from app', sessionFromLocalStorage);
-    // if (sessionFromLocalStorage) {
-    //   dispatch(loadSession(sessionFromLocalStorage.id));
-    // }
-    // if ((sessionTime, countDown)) {
-    //   const id = setInterval(() => {
-    //     setSessionTime((sessionTime) => {
-    //       localStorage.setItem('sessionTime', sessionTime - 1000);
-    //       return sessionTime - 1000;
-    //     });
-    //   }, 1000);
-    //   setIntervalID(id);
-    // }
-    // if (!countDown) {
-    //   clearInterval(intervalID);
-    //   setSessionTime(0);
-    // }
+
     chrome.runtime.sendMessage(
       'opechfjocpfdfihnebpmdbkajmmomihl',
       {
         message: 'get-time',
       },
       (response) => {
-        console.log(response);
-        localStorage.setItem('sessionTime', response.sessionTime);
-        setSessionTime(response.sessionTime);
+        if (sessionFromStorage) {
+          localStorage.setItem('sessionTime', response.sessionTime);
+          setSessionTime(response.sessionTime);
+        } else {
+          localStorage.setItem('sessionTime', 0);
+        }
       }
     );
   }, [dispatch]);
+
+  useEffect(() => {
+    // load current session if lost on reload
+    if (sessionFromStorage && !currentSession.id) {
+      dispatch(loadSession(sessionFromStorage.id));
+    }
+  }, [currentSession]);
+
+  // useEffect(() => {
+  //   if (!timeFromStorage && currentSession && sessionActive) {
+  //     props.endSession(currentSession.id, true);
+  //   }
+  // }, [timeFromStorage]);
 
   useEffect(() => {
     if (window.localStorage.getItem('token')) {
@@ -87,8 +75,6 @@ const App = (props) => {
           setSessionTime,
           goal,
           setGoal,
-          countDown,
-          setCountDown,
           intervalID,
           setIntervalID,
         }}
@@ -102,7 +88,8 @@ const App = (props) => {
 
 export default connect(null, (dispatch) => {
   return {
-    endSession: (sessionId, successful) =>
-      dispatch(endSession(sessionId, successful)),
+    endSession: (sessionId, successful) => {
+      dispatch(endSession(sessionId, successful));
+    },
   };
 })(App);
