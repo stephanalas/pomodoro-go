7bc35ee5fb0c3480b38e0f8fe6bf2a9e1e82ae06
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-08-02 23:43:05 -0400

    removed code

--- client/components/GoogleAuth.js
+++ client/components/GoogleAuth.js
@@ -13,65 +13,47 @@ const GoogleAuth = (props) => {
   const handleFail = (response) => {
     console.log('sign in failure', response);
   };
-  return (
-    <div id="extension-login">
-      <IconButton
-        className={icons}
-        id="account"
-        aria-label="menu"
-        aria-haspopup="true"
-        edge="start"
-        size="medium"
-        onClick={(ev) => setAnchorEl(ev.currentTarget)}
-      >
-        <AccountBox style={{ color: '#e0e2e4', fontSize: 30 }} />
-      </IconButton>
-
-      {props.isLoggedIn ? (
-        <GoogleLogout
-          clientId="811227993938-nd59os35t80qtuqgmul58232c54sbmsm.apps.googleusercontent.com"
-          buttonText="Logout"
-          onLogoutSuccess={handleLogOut}
-          isSignedIn={props.isLoggedIn}
-          render={(renderProps) => (
-            <Avatar
-              onClick={renderProps.onClick}
-              style={{
-                height: 30,
-                width: 30,
-                border: 0,
-                borderRadius: '50%',
-                marginTop: '10px',
-              }}
-              src="https://i.pinimg.com/originals/a3/d5/8f/a3d58f0b2820871d486e9851c0fdbb60.jpg"
-            />
-          )}
-        ></GoogleLogout>
-      ) : (
-        <GoogleLogin
-          clientId="811227993938-nd59os35t80qtuqgmul58232c54sbmsm.apps.googleusercontent.com"
-          buttonText="Login"
-          onSuccess={handleSuccess}
-          onFailure={handleFail}
-          cookiePolicy={'single_host_origin'}
-          isSignedIn={props.isLoggedIn}
-          redirectUri={`${process.env.API_URL}/home`}
-          render={(renderProps) => (
-            <Avatar
-              onClick={renderProps.onClick}
-              style={{
-                height: 30,
-                width: 30,
-                border: 0,
-                borderRadius: '50%',
-                marginTop: '10px',
-              }}
-              src="https://img-authors.flaticon.com/google.jpg"
-            />
-          )}
+  return props.isLoggedIn ? (
+    <GoogleLogout
+      clientId="811227993938-nd59os35t80qtuqgmul58232c54sbmsm.apps.googleusercontent.com"
+      buttonText="Logout"
+      onLogoutSuccess={handleLogOut}
+      isSignedIn={props.isLoggedIn}
+      render={(renderProps) => (
+        <Avatar
+          onClick={renderProps.onClick}
+          style={{
+            height: 30,
+            width: 30,
+            border: 0,
+            borderRadius: '50%',
+          }}
+          src="https://i.pinimg.com/originals/a3/d5/8f/a3d58f0b2820871d486e9851c0fdbb60.jpg"
         />
       )}
-    </div>
+    ></GoogleLogout>
+  ) : (
+    <GoogleLogin
+      clientId="811227993938-nd59os35t80qtuqgmul58232c54sbmsm.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={handleSuccess}
+      onFailure={handleFail}
+      cookiePolicy={'single_host_origin'}
+      isSignedIn={props.isLoggedIn}
+      redirectUri={`${process.env.API_URL}/home`}
+      render={(renderProps) => (
+        <Avatar
+          onClick={renderProps.onClick}
+          style={{
+            height: 30,
+            width: 30,
+            border: 0,
+            borderRadius: '50%',
+          }}
+          src="https://img-authors.flaticon.com/google.jpg"
+        />
+      )}
+    />
   );
 };
 
