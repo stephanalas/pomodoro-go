3bf1fb5b1370d9a4d20648c197988aa0390e6dbf
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-08-02 23:44:11 -0400

    deprecated

--- client/components/WebsiteNav.js
+++ client/components/WebsiteNav.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { AppBar, Toolbar } from '@material-ui/core';
+import { IconButton, Avatar } from '@material-ui/core';
+import { AccountBox } from '@material-ui/icons';
+
 import MainLogo from './MainLogo';
 import PublicNav from './PublicNav';
 import LoggedInNav from './LoggedInNav';
@@ -20,26 +23,69 @@ const WebsiteNav = (props) => {
       className={headerStyle}
       style={{ backgroundColor: '#5061a9' }}
     >
-      <Toolbar>
-        <MainLogo />
-        {isLoggedIn ? (
-          <LoggedInNav
-            setAnchorEl={setAnchorEl}
-            anchorEl={anchorEl}
-            handleLogOut={handleLogOut}
-            icons={icons}
-          />
-        ) : (
-          <PublicNav anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
-        )}
-        <GoogleAuth
-          setAnchorEl={setAnchorEl}
-          handleLogOut={handleLogOut}
-          getMe={getMe}
-          isLoggedIn={isLoggedIn}
-          icons={icons}
-        />
-      </Toolbar>
+      {isLoggedIn ? (
+        <Toolbar
+          children={[
+            <MainLogo />,
+            <LoggedInNav
+              setAnchorEl={setAnchorEl}
+              anchorEl={anchorEl}
+              handleLogOut={handleLogOut}
+              icons={icons}
+            />,
+            <IconButton
+              className={icons}
+              id="account"
+              aria-label="menu"
+              aria-haspopup="true"
+              edge="start"
+              size="medium"
+              onClick={(ev) => setAnchorEl(ev.currentTarget)}
+            >
+              <AccountBox style={{ color: '#e0e2e4', fontSize: 30 }} />
+            </IconButton>,
+            <GoogleAuth
+              setAnchorEl={setAnchorEl}
+              handleLogOut={handleLogOut}
+              getMe={getMe}
+              isLoggedIn={isLoggedIn}
+              icons={icons}
+            />,
+          ]}
+        ></Toolbar>
+      ) : (
+        <Toolbar
+          children={[
+            <MainLogo />,
+            <PublicNav anchorEl={anchorEl} setAnchorEl={setAnchorEl} />,
+            <IconButton
+              className={icons}
+              id="account"
+              aria-label="menu"
+              aria-haspopup="true"
+              edge="start"
+              size="medium"
+              onClick={(ev) => setAnchorEl(ev.currentTarget)}
+            >
+              <AccountBox style={{ color: '#e0e2e4', fontSize: 30 }} />
+            </IconButton>,
+            <GoogleAuth
+              setAnchorEl={setAnchorEl}
+              handleLogOut={handleLogOut}
+              getMe={getMe}
+              isLoggedIn={isLoggedIn}
+              icons={icons}
+            />,
+          ]}
+        ></Toolbar>
+      )}
+      {/* <GoogleAuth
+        setAnchorEl={setAnchorEl}
+        handleLogOut={handleLogOut}
+        getMe={getMe}
+        isLoggedIn={isLoggedIn}
+        icons={icons}
+      /> */}
     </AppBar>
   );
 };
